8317e851074f90b618a09eeea9df878fbde2d995
bobibi <bobibi@users.noreply.github.com>
2015-05-31 21:08:26 -0700

    Update ContainsNearbyAlmostDuplicate.hh

--- Heap/ContainsNearbyAlmostDuplicate.hh
+++ Heap/ContainsNearbyAlmostDuplicate.hh
@@ -1,3 +1,18 @@
+/**
+ * Contains Duplicate III
+ * 
+ * Question: Given an array of integers, find out whether there are two distinct indices i and j in the array 
+ * such that the difference between nums[i] and nums[j] is at most t and the difference between i and j is at 
+ * most k.
+ * 
+ * Solution: Maintain a sorted structure which contains at most consecutive k elements of nums, i.e. a moving
+ * window with maximum size k. Iterating over nums, compare the distance between the coming number and the max
+ * and min of the moving window. If the distance is less than or equal to t, then return TRUE. Insert the coming
+ * number, and remove the oldest number if the window size exceeds k.
+ * 
+ * Be careful about overflowing.
+**/
+
 class Solution {
 public:
     bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
