667073c422471df3649dcab1403c6afb806edc0d
bobibi <bobibi@users.noreply.github.com>
2015-05-31 20:50:06 -0700

    Create ContainsNearbyAlmostDuplicate.hh

---
+++ Heap/ContainsNearbyAlmostDuplicate.hh
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
+        if(nums.size()<2 || k<1 || t<0) return false;
+        set<int> m;
+        for(int i=0; i<k&&i<nums.size(); i++) {
+            if(m.size()>0 && (diff(nums[i], *(m.begin()))<=t || diff(nums[i], *(m.rbegin()))<=t)) return true;
+            m.insert(nums[i]);
+        }
+        for(int i=k; i<nums.size(); i++) {
+            if(diff(nums[i], *(m.begin()))<=t || diff(nums[i], *(m.rbegin()))<=t) return true;
+            m.insert(nums[i]);
+            m.erase(nums[i-k]);
+        }
+        return false;
+    }
+    
+    long diff(long a, long b) {
+        return a>b?a-b:b-a;
+    }
+};
